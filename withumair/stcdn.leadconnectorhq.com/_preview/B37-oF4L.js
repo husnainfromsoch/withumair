75380e3ca5e8d732fa93ff16ede8b3f5dc88ca01
Husnain Ali <husnainpkofficial@gmail.com>
2026-06-22 14:24:01 +0500

    withumair site

---
+++ withumair/stcdn.leadconnectorhq.com/_preview/B37-oF4L.js
@@ -0,0 +1,1 @@
+import{F as u}from"./DUIwg4bG.js";import{h as l}from"./Bfx9ueE_.js";let e=null;const c=()=>{const t=l(),r=t.value.geoCountry;return r&&r!=="ZZ"?Promise.resolve(r):(e||(e=u.getGeoLocation().then(o=>{const n=o?.country??null;return n&&(t.value.geoCountry=n),n}).catch(o=>null).finally(()=>{e=null})),e)};export{c as e};
